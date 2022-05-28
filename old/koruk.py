be5496ad52851aa065205cf6e33ae88a8941910a
Selcan Yağmur Atak <73903096+slcnyagmurnew@users.noreply.github.com>
2022-05-29 02:13:11 +0300

    Delete old directory

--- old/koruk.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import numpy as np
-img = Image.open(f'images/circle.png').convert('L')
-image = np.array(img)
-for i in image:
-    print(i)
-exit(3)
-img = img.resize((10, 10))
